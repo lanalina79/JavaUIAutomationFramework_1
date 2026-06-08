06fc3d49374c41ad85d49b581762b3efefcabfc7
Elena Iliina <elena.iliina@gmail.com>
2026-06-08 23:18:05 +0300

    Added the Implementation for the Driver Manager

--- src/main/java/online/opencart_automation/TestRunner.java
+++ src/main/java/online/opencart_automation/TestRunner.java
@@ -1,17 +1,30 @@
 package online.opencart_automation;
 
+import online.opencart_automation.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class TestRunner {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException {
+       // DriverManager manager= DriverManager.getInstance();
+       // WebDriver driver=manager.getDriver();
+        //or
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        WebDriver driver = DriverManager.getInstance().getDriver();
+        String currentTabName= driver.getWindowHandle();
+        Thread.sleep(5000);
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-safari-driver/4.43.0");
+        driver.close();
+        driver.switchTo().window(currentTabName);
+        driver.get("https://ru.wikipedia.org/wiki/%D0%A2%D0%B5%D1%80%D0%BC%D0%B8%D0%BD%D0%B0%D1%82%D0%BE%D1%80_2:_%D0%A1%D1%83%D0%B4%D0%BD%D1%8B%D0%B9_%D0%B4%D0%B5%D0%BD%D1%8C");
+        Thread.sleep(10000);
+        driver.quit();
+
+
+        System.out.println("The test is finished and the driver is closed");
     }
+
 }
